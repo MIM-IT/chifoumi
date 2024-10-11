4fd580a64125283519d88e479e19ac8383fd49c2
MIM-IT <contact.marika.maggioli@gmail.com>
2024-10-11 10:41:52 +0200

    ajout des modalités d'entrée dans le jeu

--- src/chifoumi/Chifoumi.java
+++ src/chifoumi/Chifoumi.java
@@ -1,10 +1,29 @@
 package chifoumi;
+import java.util.Scanner;
 
 public class Chifoumi {
+	
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Combien de tour  ?");
+		String tour = sc.next();
+		System.out.println("Vous allez jouer " + tour + " tour(s).");
+		
+		String version = "";
+			
+		while( !(version.equalsIgnoreCase("1") || version.equalsIgnoreCase("2"))) {
+			System.out.println("Deux versions {classique/spock}. 1 ou 2 ?");
+			version = sc.next();
+			
+			if(  !(version.equalsIgnoreCase("1") || version.equalsIgnoreCase("2")) ) {
+				// Si version n'est pas égal à 1 ou 2
+				System.out.println("Ce n'est pas bon re essayez !");
+			}
+		}
 	}
 
 }
